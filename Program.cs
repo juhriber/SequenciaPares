660d3fac8d8aca606bf13f4ffe2d7e3c00a4a3b1
juhriber <j.de.s.r.25@gmail.com>
2021-05-16 16:59:29 -0300

    Finalização projeto SequenciaPares

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SequenciaPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+
+            Console.Write("Digite um Número: ");
+            valor = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < valor; i = i + 2)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+        }
+    }
+}
